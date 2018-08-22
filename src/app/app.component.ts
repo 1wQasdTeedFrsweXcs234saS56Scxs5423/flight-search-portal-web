707117ce56702a31da8eec777aee6781ae8407c7
Ankit Tanna <ankittanna@hotmail.com>
2018-08-23 02:07:12 +0530

    [NO-STORY] | UI | Component Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
-/**
- * Angular 2 decorators and services
- */
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { environment } from 'environments/environment';
 import { AppState } from './app.service';
 
 export const ROOT_SELECTOR = 'app';
 
-/**
- * App Component
- * Top Level Component
- */
 @Component({
   selector: ROOT_SELECTOR,
   encapsulation: ViewEncapsulation.None,
@@ -35,11 +28,3 @@ export class AppComponent implements OnInit {
   }
 
 }
-
-/**
- * Please review the https://github.com/AngularClass/angular-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
