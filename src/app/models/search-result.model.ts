1c88d686c28ff06d1e469075084330ef420a677d
Ankit Tanna <ankittanna@hotmail.com>
2018-10-23 22:55:45 +0530

    [SEARCH-REQUEST] | Search result model added

--- src/app/models/search-result.model.ts
+++ src/app/models/search-result.model.ts
@@ -1,3 +1,27 @@
 export class SearchResultModel {
-  public hello: string = '';
+  public operator: string;
+  public flightNumber: string;
+  public departureDate: string;
+  public departureTime: string;
+  public duration: string;
+  public isMealIncluded: boolean;
+  public departureCity: string;
+  public arrivalCity: string;
+  public fare: number;
+  public fareCurrency: string;
+  public seatsLeft: number;
+
+  constructor() {
+    this.operator = '';
+    this.flightNumber = '';
+    this.departureDate = '';
+    this.departureTime = '';
+    this.duration = '';
+    this.isMealIncluded = false;
+    this.departureCity = '';
+    this.arrivalCity = '';
+    this.fare = 0;
+    this.fareCurrency = '';
+    this.seatsLeft = 0;
+  }
 }
